f04cfaf87e8aa860850f222b0d3c00952a6ab3f2
Akshaysarange <akshaysarange8@gmail.com>
2026-01-18 21:20:01 +0530

    4th

--- Contact_number.py
+++ Contact_number.py
@@ -1,5 +1,7 @@
 print('----------Contacts_App----------')
 
+name_list = [contact_name]
+
 while True:
     print('''Your Options:-
     1. Add contact
@@ -13,8 +15,18 @@ while True:
         print('Goodbye')
         break
     elif op == 1:
-        contact_name = input('Enter name:-').strip()
+        contact_name = input('Enter name:-').strip().capitalize()
         contact_number = int(input('Enter contact number:-'))
+
+        with open('contact_list.txt', 'a') as file:
+            file.write(f'Name:- {contact_name}    Contact_number:- {contact_number}\n')
+            print(f'{contact_name} contact details add successfully!!!')
+            
+    elif op == 3:
+        with open('contact_list.txt', 'r') as file:
+            print(file.readlines())
+    elif op == 4:
+        contact_name = input('Enter name:-').strip().capitalize()
 
         with open('contact_list.txt', 'a') as file:
             file.write(f'Name:- {contact_name}    Contact_number:- {contact_number}\n')
